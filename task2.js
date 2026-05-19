23b33812f04fff99d0fba830217ff1a8f4e0b7ba
kiruba2026 <kirubakaranr@thestackly.com>
2026-05-19 11:22:36 +0530

    Added task2.js

---
+++ task2.js
@@ -0,0 +1,258 @@
+// 1. Create a variable and store your favorite food.
+
+let favoriteFood = "Biriyani"
+console.log(favoriteFood)
+
+
+// 2. Create a variable and store your mobile number.
+
+let mobileNumber = "9876543210"
+console.log(mobileNumber)
+
+
+// 3. Create a boolean variable with false value.
+
+let isPassed = false
+console.log(isPassed)
+
+
+// 4. Create one undefined variable and print typeof.
+
+let data
+console.log(typeof data)
+
+
+// 5. Create one array with 6 animal names.
+
+let animals = ["Lion", "Tiger", "Elephant", "Monkey", "Dog", "Cat"]
+console.log(animals)
+
+
+// 6. Print second animal from array.
+
+console.log(animals[1])
+
+
+// 7. Print last animal from array using length.
+
+console.log(animals[animals.length - 1])
+
+
+// 8. Create object with bike details (name, model, color).
+
+let bike = {
+    name: "Yamaha",
+    model: "R15",
+    color: "Black"
+}
+
+console.log(bike)
+
+
+// 9. Print bike color from object.
+
+console.log(bike.color)
+
+
+// 10. Create two variables and subtract values.
+
+let num1 = 50
+let num2 = 20
+
+console.log(num1 - num2)
+
+
+// 11. Create two variables and divide values.
+
+let value1 = 100
+let value2 = 5
+
+console.log(value1 / value2)
+
+
+// 12. Find 15 % 2.
+
+console.log(15 % 2)
+
+
+// 13. Find 2 ** 5.
+
+console.log(2 ** 5)
+
+
+// 14. Create variable with 5 and use post increment.
+
+let a = 5
+console.log(a++)
+console.log(a)
+
+
+// 15. Create variable with 10 and use pre increment.
+
+let b = 10
+console.log(++b)
+
+
+// 16. Create variable with 20 and use post decrement.
+
+let c = 20
+console.log(c--)
+console.log(c)
+
+
+// 17. Create variable with 50 and use pre decrement.
+
+let d = 50
+console.log(--d)
+
+
+// 18. Check 100 > 50.
+
+console.log(100 > 50)
+
+
+// 19. Check 25 < 10.
+
+console.log(25 < 10)
+
+
+// 20. Check 50 >= 50.
+
+console.log(50 >= 50)
+
+
+// 21. Check 30 <= 25.
+
+console.log(30 <= 25)
+
+
+// 22. Check 10 == "10".
+
+console.log(10 == "10")
+
+
+// 23. Check 10 === "10".
+
+console.log(10 === "10")
+
+
+// 24. Check 25 != "25".
+
+console.log(25 != "25")
+
+
+// 25. Check 25 !== "25".
+
+console.log(25 !== "25")
+
+
+// 26. Use AND operator:
+// 10 > 5 && 20 > 15
+
+console.log(10 > 5 && 20 > 15)
+
+
+// 27. Use AND operator:
+// 5 > 10 && 20 > 15
+
+console.log(5 > 10 && 20 > 15)
+
+
+// 28. Use OR operator:
+// 5 > 10 || 20 > 15
+
+console.log(5 > 10 || 20 > 15)
+
+
+// 29. Use OR operator:
+// 2 > 5 || 1 > 10
+
+console.log(2 > 5 || 1 > 10)
+
+
+// 30. Use NOT operator:
+// !(10 > 5)
+
+console.log(!(10 > 5))
+
+
+// 31. Use NOT operator:
+// !(5 > 10)
+
+console.log(!(5 > 10))
+
+
+// 32. Create ternary operator:
+// if 50 > 25 print "True"
+// else print "False"
+
+let result1 = 50 > 25 ? "True" : "False"
+
+console.log(result1)
+
+
+// 33. Create ternary operator:
+// if 18 >= 21 print "Adult"
+// else print "Minor"
+
+let result2 = 18 >= 21 ? "Adult" : "Minor"
+
+console.log(result2)
+
+
+// 34. Create one array with 5 city names and print third city.
+
+let cities = ["Chennai", "Madurai", "Coimbatore", "Trichy", "Salem"]
+
+console.log(cities[2])
+
+
+// 35. Create one object with employee details and print employee name.
+
+let employee = {
+    name: "Kiruba",
+    age: 22,
+    department: "IT"
+}
+
+console.log(employee.name)
+
+
+// 36. Create variable with string "100" and print typeof.
+
+let numberString = "100"
+
+console.log(typeof numberString)
+
+
+// 37. Create variable with number 100 and print typeof.
+
+let numberValue = 100
+
+console.log(typeof numberValue)
+
+
+// 38. Create one null variable and print it.
+
+let emptyValue = null
+
+console.log(emptyValue)
+
+
+// 39. Create one array with mixed datatypes.
+
+let mixedData = ["Kiruba", 22, true, null, 3.14]
+
+console.log(mixedData)
+
+
+// 40. Create object with student details:
+// name, mark, passed(boolean)
+
+let student = {
+    name: "Kiruba",
+    mark: 85,
+    passed: true
+}
+
+console.log(student)
